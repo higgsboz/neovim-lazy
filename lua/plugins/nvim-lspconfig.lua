539b47995f60eac24b3b3c8adeda78c66ac96dc3
higgsboz-beam <brandon.minner@beam.dental>
2025-06-09 14:02:19 -0400

    feat(plugins): add CodeCompanion.nvim integration
    - Add codecompanion.nvim plugin configuration with Claude 3.7 Sonnet
    model
    - Add keybindings for CodeCompanion actions and chat- Set up command
    alias 'cc' for CodeCompanion
    - Disable hardtime.nvim by default- Update mason configuration to avoid
    managing erb-formatter
    - Add commented-out code for potential conform.nvim erb formatting
    solutions
    - Update various plugin dependencies to their latest versions

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -32,3 +32,55 @@ return {
     },
   },
 }
+
+-- Possible solution to have erb_lint work with ruby_lsp
+
+-- return {
+--   "neovim/nvim-lspconfig",
+--   opts = {
+--     servers = {
+--       -- Your existing configuration
+--       ruby_lsp = {
+--         mason = false,
+--         cmd = { "/opt/homebrew/bin/mise", "x", "--", "ruby-lsp" },
+--         root_dir = util.root_pattern(".git"),
+--         init_options = {
+--           formatter = "rubocop",
+--           enabledFeatures = {
+--             "codeActions",
+--             "diagnostics",
+--             "documentHighlights",
+--             "documentLink",
+--             "documentSymbols",
+--             "foldingRanges",
+--             "formatting",
+--             "hover",
+--             "inlayHint",
+--             "onTypeFormatting",
+--             "selectionRanges",
+--             "semanticHighlighting",
+--             "completion",
+--             "codeLens",
+--             "definition",
+--             "workspaceSymbol"
+--           },
+--           -- Add this section to enable ERB-Lint integration
+--           experimentalFeaturesEnabled = true,
+--           embeddedLanguages = {
+--             erb = {
+--               enabled = true,
+--               formatter = {
+--                 command = "/opt/homebrew/bin/mise",
+--                 args = { "exec", "ruby", "--", "erb_lint", "--autocorrect", "--stdin", "${INPUT}" },
+--               },
+--               linter = {
+--                 command = "/opt/homebrew/bin/mise",
+--                 args = { "exec", "ruby", "--", "erb_lint", "--lint", "${FILE}" },
+--               },
+--             },
+--           },
+--         },
+--       },
+--     },
+--   },
+-- }
