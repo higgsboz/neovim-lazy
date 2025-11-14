0c377fd0269cc1925f9b676983ed8dc7596ef6c0
Brandon Minner <bminner4@gmail.com>
2025-11-14 10:08:27 -0500

    diffview lazy loading

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,5 +1,6 @@
 return {
   "sindrets/diffview.nvim",
+  lazy = false,
   keys = {
     { "<leader>gdo", "<cmd>DiffviewOpen<CR>", desc = "Open Diff View" },
     { "<leader>gdc", "<cmd>DiffviewClose<CR>", desc = "Close Diff View" },
